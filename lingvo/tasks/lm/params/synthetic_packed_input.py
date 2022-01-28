275ff1306605f5f65ba5e6ede895f769088a5147
chadwickleung <chadwickleung.wcl@gmail.com>
2022-01-27 19:54:21 -0800

    changed num_devices = 4

--- lingvo/tasks/lm/params/synthetic_packed_input.py
+++ lingvo/tasks/lm/params/synthetic_packed_input.py
@@ -107,7 +107,7 @@ class DenseLmTemplate(base_model_params.SingleTaskModelParams):
             atten_logit_cap=self.ATTEN_LOGIT_CAP,
             attention_logits_dtype=tf.float32,
             dropout_rate=0.0,
-            num_devices=8,  # Obsolete params, originally = 1
+            num_devices=4,  # Obsolete params, originally = 1
             attention_dropout_prob=0.0,
             attention_key_value_dim=self.ATTENTION_KEY_VALUE_DIM,
             attention_extra_logit=0.0,
