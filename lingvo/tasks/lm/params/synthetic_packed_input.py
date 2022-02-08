52443e8337a8973a30cff4521957480e9f36b1aa
chadwickleung <chadwickleung.wcl@gmail.com>
2022-02-07 17:32:48 -0800

    chores

--- lingvo/tasks/lm/params/synthetic_packed_input.py
+++ lingvo/tasks/lm/params/synthetic_packed_input.py
@@ -121,7 +121,7 @@ class DenseLmTemplate(base_model_params.SingleTaskModelParams):
             ff_dim=self.HIDDEN_DIM,
             attention_combine_dims=True,
             moe_hidden_dim = self.MOE_HIDDEN_DIM if self.MOE else None,
-            capacity_factor = 4 if self.MOE else None,
+            capacity_factor = 1 if self.MOE else None,
             # second_expert_policy = 'sampling' if self.MOE else None,  # Uses 'all' if not specify
             num_groups = 2 if self.MOE else None,  # Chadwick: Code was not using num_groups, they use num_devices == 1
             e_dim = self.NUM_DEVICES_PER_SPLIT if self.MOE else None,  # Chadwick: number of experts
